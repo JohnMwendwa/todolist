4a626f55a390041fce2685be25cb163f1fcf1134
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-05-22 21:40:10 +0300

    import TodoApp stylesheet

--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import AddTodoForm from './AddTodoForm'
 import TodoList from './TodoList'
+import './TodoApp.css'
 
 function TodoApp() {
   return (
-    <div>
+    <div className='TodoApp'>
+      <h1>TodoList
+     <span>A simple React TodoList App</span>
+     </h1>
       <AddTodoForm />
       <TodoList />
     </div>
