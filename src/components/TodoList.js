bc10f1d7f50bcd979dd82f9f852b9cbe0fbc1548
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-05-22 15:16:53 +0300

    render a list of todos

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import Todo from './Todo'
 
 function TodoList() {
   return (
     <div>
-        
+        {todos.map(todo =>(
+            <Todo 
+            id={todo.id}
+            todo={todo.task}
+            />
+        ))}
     </div>
   )
 }
