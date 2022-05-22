006b236e87ea8b36bdff95a53e050e2c644f208e
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-05-22 16:28:28 +0300

    add remove method to remove todos

--- src/reducer/todos.reducer.js
+++ src/reducer/todos.reducer.js
@@ -4,7 +4,8 @@ switch(action.type){
     //Add todos method
     case "ADD":
     return [...state,action.task];
-    
+    case "REMOVE":
+        return state.filter(todo=>todo.id !== action.id );
 
 }
 }
