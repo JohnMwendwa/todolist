f84be37f875ce34b111393ddab6dd636d8ee63fe
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-05-22 16:47:23 +0300

    separate the dispatch and todos context to improve app performance and avoid unnecessary re-renders

---
+++ src/context/todos.context.js
@@ -0,0 +1,23 @@
+import React, { createContext} from 'react';
+import useLocalStorageReducer from '../hooks/useLocalStorageReducer';
+import todosReducer from '../context/todos.context'
+
+const defaultTodos = [
+    {id:1,task:"Learn coding", completed:false},
+    {id:2, task:"Go shopping at the mall",completed:false}
+];
+
+export const TodosContext = createContext();
+export const DispatchContext = createContext();
+
+export function TodosProvider(props) {
+    const [todos,dispatch] = useLocalStorageReducer("Tasks",defaultTodos,todosReducer);
+
+  return (
+  <TodosContext.Provider value={todos}>
+      <DispatchContext.Provider value={dispatch}>
+         {props.children}
+      </DispatchContext.Provider>
+  </TodosContext.Provider>
+
+  )}
