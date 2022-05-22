a7874e3d0f76bcacbbce3c6a5934b0ee0625d8a1
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-05-22 21:30:44 +0300

    import TodoApp stylesheet

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,18 +1,19 @@
 import React, { useContext } from 'react'
 import Todo from './Todo'
 import {TodosContext} from '../context/todos.context'
-
+import './TodoList.css';
 function TodoList() {
     const todos = useContext(TodosContext);
+    const tasks = todos.map(todo =>(
+      <Todo 
+      key={todo.id}
+      id={todo.id}
+      todo={todo.task}
+      />
+  ))
   return (
-    <div>
-        {todos.map(todo =>(
-            <Todo 
-            key={todo.id}
-            id={todo.id}
-            todo={todo.task}
-            />
-        ))}
+    <div className='TodoList'>
+     <ul>{tasks}</ul>
     </div>
   )
 }
