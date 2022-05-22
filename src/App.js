52b50cddfd5212d5f4744c71922a7c2e8191b93b
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-05-22 16:50:40 +0300

    wrap the whole TodoApp component with the TodosProvider context

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import TodoApp from './components/TodoApp';
+import {TodosProvider} from './context/todos.context'
 
 function App() {
   return (
     <div className="App">
-     <TodoApp />
+      <TodosProvider>
+          <TodoApp />
+     </TodosProvider>
     </div>
   );
 }
