5bb74c532b91c524e40b7327fc2e9f394fe3ab13
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-05-22 16:40:13 +0300

    add a toggle method to toggle completed tasks and a default method when nothing matches

--- src/reducer/todos.reducer.js
+++ src/reducer/todos.reducer.js
@@ -8,6 +8,10 @@ switch(action.type){
         return state.filter(todo=>todo.id !== action.id );
     case "EDIT":
         return state.map(todo => todo.id === action.id ? {...todo,task:action.task} : todo);
+    case "TOGGLE":
+        return state.map(todo => todo.id === action.id ? {...todo,completed:!todo.completed} : todo);
+     default:
+        return state;
 }
 }
 
