4575da2c90f7d37de46f306d58f4703dfcee9ed6
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-05-24 19:31:19 +0300

    add a toggle complete button

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,16 +3,25 @@ import {DispatchContext} from '../context/todos.context';
 import EditTodoForm from './EditTodoForm';
 import './Todo.css';
 
-function Todo({id,todo}) {
+function Todo({id,todo,completed}) {
     const dispatch = useContext(DispatchContext);
     const [isEdit,setIsEdit] = useState(false);
     const handleEdit =()=>{
         setIsEdit(!isEdit)
     }
+
   return (
         isEdit ? <EditTodoForm  id={id} todo={todo} toggleEdit={handleEdit}  /> :
      <div className='Todo'>
-        <li>
+
+<button 
+className='Todo-toggle-btn'
+onClick={()=>dispatch({type:"TOGGLE",id:id})}
+>
+  <i className={completed ? "fas fa-check-square fa-2x" : "fas fa-square fa-2x" }/>
+</button>
+
+        <li className={completed ? "Todo-task completed" : '' }  >
           {todo}
           </li>
       <div className='Todo-buttons'>
